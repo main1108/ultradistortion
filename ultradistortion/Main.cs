deca39f3c0704ae8f219e2d6671788c5825c4dca
main1108 <main1108@i.softbank.jp>
2024-04-30 01:19:14 +0900

    bump version

--- ultradistortion/Main.cs
+++ ultradistortion/Main.cs
@@ -33,7 +33,7 @@ namespace ultradistortion
         public static bool isthereconfigManager;
         private const string Guid = "susinopo.ULTRADistortion";
         private const string InternalName = "ULTRADistortion";
-        private const string InternalVersion = "0.0.1";
+        private const string InternalVersion = "0.0.2";
         public void SetConfig()
         {
             audioIntensityValue = audiodistortionlevel.Value;
